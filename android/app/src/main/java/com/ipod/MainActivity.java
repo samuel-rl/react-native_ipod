ad79817285ddefef4f6dcc86c1083f8994ee3f9e
Samuel <samuel310597@gmail.com>
2021-02-14 13:27:23 +0100

    init + add dependencies + linked to Android

---
+++ android/app/src/main/java/com/ipod/MainActivity.java
@@ -0,0 +1,25 @@
+package com.ipod;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ipod";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+}
